eee83b86deef3fe77d3036601a465ec64ea7b4d2
Vishvendra Singh <ddmteetu@gmail.com>
2021-03-18 18:56:55 +0530

    New migration - AddMigrationType - against a new model, MembershipType , added

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,5 +10,9 @@ namespace Vidly.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
+        //Associate `Customer` Class with `MembershipType` with the help of a navigational property, `MembershipType`
+        public MembershipType MembershipType { get; set; }
+        //`Foreign Key` to the `MembershipType` Table
+        public byte MembershipTypeId { get; set; }
     }
 }
