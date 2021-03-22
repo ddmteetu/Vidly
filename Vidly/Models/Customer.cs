b60eedde42730834ca01715717a2050d68118132
Vishvendra Singh <ddmteetu@gmail.com>
2021-03-22 14:50:05 +0530

    Solution - Section 3 Exercise - Display customer's birthdate on their profile page

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -12,6 +12,7 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         //Associate `Customer` Class with `MembershipType` with the help of a navigational property, `MembershipType`
         public MembershipType MembershipType { get; set; }
