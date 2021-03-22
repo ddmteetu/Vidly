d01db15c3e2a0ea028eaab4b70efdc1632632f97
Vishvendra Singh <ddmteetu@gmail.com>
2021-03-22 07:47:45 +0530

    Eager Loading - loading Customer objects with their related objects (MembershipTypes) and updating the view as well

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -35,7 +36,7 @@ namespace Vidly.Controllers
 
         public ViewResult Index()
         {
-            var customers = _context.Customers;
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
