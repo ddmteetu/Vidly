c4509707945ae99d784a9a23fb9cd8c5451810e1
Vishvendra Singh <ddmteetu@gmail.com>
2021-03-26 13:09:50 +0530

    Add a new customer to the list of customers using a form

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -20,6 +21,26 @@ namespace Vidly.Controllers
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
+        }
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
         }
 
         // GET: Customers
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,14 +9,21 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        
+        [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
+        
         public bool IsSubscribedToNewsletter { get; set; }
         //Associate `Customer` Class with `MembershipType` with the help of a navigational property, `MembershipType`
+        
         public MembershipType MembershipType { get; set; }
         //`Foreign Key` to the `MembershipType` Table
+        
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
     }
 }
