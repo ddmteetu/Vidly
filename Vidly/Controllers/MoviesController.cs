36f848688f112ff026ad1818badcddd13cb5236a
Vishvendra Singh <ddmteetu@gmail.com>
2021-03-26 19:09:06 +0530

    Solution - Section 4 Exercise - Add a form to create/edit a movie

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -22,6 +23,65 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+
+            _context.SaveChanges();
+            /*try
+            {
+                _context.SaveChanges();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+            }*/
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+        
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -40,11 +100,6 @@ namespace Vidly.Controllers
             };
 
             return View(viewModel);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
         }
 
         // movies
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,15 +9,24 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+        
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+        
         public DateTime DateAdded { get; set; }
+        
+        [Display(Name = "Number in Stock")]
         public int NumberInStock { get; set; }
+        
         //Associate `Movie` Class with `Genre` with the help of a navigational property, `Genre`
         public Genre Genre { get; set; }
+        
         //`Foreign Key` to the `Genre` Table
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
     }
 }
